c1b32044c7d282d9009164b63868505364e7a9d0
Jueun Mun <jueun@Jueuns-MacBook-Pro.local>
2023-04-27 22:00:18 +0900

    Add rendering origin

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
   --root_dir ./data/sacre_coeur/ --dataset_name phototourism \
   --img_downscale 2 --use_cache --N_importance 64 --N_samples 64 \
    --N_vocab 1500 \
-  --num_epochs 20 --batch_size 2048 \
+  --num_epochs 10 --batch_size 1024 \
   --optimizer adam --lr 5e-4 --lr_scheduler cosine \
   --root_dir ./data/sacre_coeur/ --dataset_name phototourism \
-  --exp_name sacre_coeur_scale2  --num_gpus 2
+  --exp_name sacre_coeur_scale2  --num_gpus 1
